e74bc7eb0949f526d62f7f0d6fb61486dfb2e72e
amruta733833-art <amruta733833@gmail.com>
2025-12-11 22:52:28 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,6 +1,6 @@
 try:
   a = float(input("Enter first number:"))
-  b = float(input("Enter second number:))
+  b = float(input("Enter second number:"))
 
   print(f"Addition:{a + b}")
   print(f"Subtraction:{a - b}")
